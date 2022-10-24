fc7a985c43c59db3a7719d8bbae1e77221db3051
eulaapp <pereiraeulapaula@gmail.com>
2022-10-24 16:38:14 -0300

    [ADD] script.js

---
+++ HTML_e_CSS/Atividades/05/script.js
@@ -0,0 +1,41 @@
+function converterDolarReal() {
+    let valor = document.getElementById("valor");
+    let dolar = 5.2744;
+
+    alert((dolar * valor.value).toFixed(3))
+}
+
+function converterRealDolar() {
+    let valor = document.getElementById("valor");
+    let real = 0.189595;
+
+    alert((valor.value * real).toFixed(3))
+}
+
+function converterEuroDolar() {
+    let valor = document.getElementById("valor");
+    let euro = 0.9871;
+
+    alert((valor.value * euro).toFixed(3))
+}
+
+function converterRealEuro() {
+    let valor = document.getElementById("valor");
+    let real = 0.1920529;
+
+    alert((valor.value * real).toFixed(3))
+}
+
+function converterLibraReal() {
+    let valor = document.getElementById("valor");
+    let libra = 5.9541;
+
+    alert((valor.value * libra).toFixed(3))
+}
+
+function converterRealLibra() {
+    let valor = document.getElementById("valor");
+    let real =  0.1679177;
+
+    alert((valor.value * real).toFixed(3))
+}
