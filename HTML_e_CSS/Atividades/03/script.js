5c6e520af51223d59c1a177d95b8129970a947f5
eulaapp <pereiraeulapaula@gmail.com>
2022-10-24 15:05:38 -0300

    [ADD] script.js

---
+++ HTML_e_CSS/Atividades/03/script.js
@@ -0,0 +1,44 @@
+function selecionarCidade(cidade) {
+    switch(cidade) {
+        case "valor1": 
+            document.getElementById('descricao').innerText = "Blumenau é um município do estado de Santa Catarina, Região Sul do Brasil. Localiza-se na microrregião homônima e na Mesorregião do Vale do Itajaí. É a cidade-sede da Região Metropolitana do Vale do Itajaí. É o terceiro município mais populoso do estado, o 8º da Região Sul do Brasil, o 78º do Brasil e a única cidade média-grande de Santa Catarina, constituindo um de seus principais polos industriais, tecnológicos e universitários.";
+        break;
+
+        case "valor2":
+            document.getElementById('descricao').innerText = "Gaspar é um município brasileiro do estado de Santa Catarina, região Sul. Localiza-se na região do Vale do Itajaí, entre Blumenau e Itajaí. Sua população em 2019 é de 69.639 habitantes, segundo estimativas do IBGE.";
+        break;
+
+        case "valor3":
+            document.getElementById('descricao').innerText = "Brusque é um município brasileiro do estado de Santa Catarina. Localiza-se na microrregião de Blumenau e na Mesorregião do Vale do Itajaí. Sua população estimada em 2019 é de 134.723[3] habitantes, sendo a 12ª maior do estado. Possui uma área de 283,446 km².[6]";
+        break;
+
+        case "valor4":
+            document.getElementById('descricao').innerText = "Uma nova comunidade, criada a seis quilômetros da Armação por moradores deslocados de núcleos de Itapocoroy, teve progresso suficiente para se elevada à categoria de freguesia em 23 de março de 1839, com o nome de Freguesia de Nossa Senhora da Penha do Itapocoróy. No século XIX a caça da baleia entrou em crise e foi substituída pela pesca artesanal e comércio rudimentar como subsistência.";
+        break;
+
+        case "valor5":
+            document.getElementById('descricao').innerText = "Florianópolis é a capital do estado brasileiro de Santa Catarina, na Região Sul do país. O município é composto pela ilha principal, a ilha de Santa Catarina, a parte continental e algumas pequenas ilhas circundantes. A cidade tem uma população de 516 524 habitantes, de acordo com estimativas para 2021 do Instituto Brasileiro de Geografia e Estatística (IBGE).";
+        break;
+
+        case "valor6":
+            document.getElementById('descricao').innerText = "Joinville é um município localizado na região norte do estado de Santa Catarina. Sua população, conforme estimativas do IBGE de 2021, era de 604 708 habitantes[6], sendo a maior cidade do estado, à frente da capital Florianópolis, e é a terceira mais populosa cidade da Região Sul do Brasil atrás apenas de Porto Alegre e Curitiba (da qual é distante apenas 130 quilômetros, sendo assim, mais próxima da capital paranaense do que a do próprio estado)";
+        break;
+
+        case "valor7":
+            document.getElementById('descricao').innerText = "Curitiba é um município brasileiro, capital do estado do Paraná, localizado a 934 metros de altitude no Primeiro Planalto Paranaense,[8] a mais de 110 quilômetros do Oceano Atlântico,[12] distante 1 386 km a sul de Brasília, capital federal. Com 1 963 726 habitantes,[1] é o município mais populoso do Paraná e da região Sul, além de ser o 8.º do país, segundo estimativa populacional calculada pelo IBGE para 2021. Fundado em 1693, a partir de um pequeno povoado bandeirante, Curitiba tornou-se uma importante parada comercial com a abertura da estrada tropeira entre Sorocaba e Viamão,[13] vindo, em 1853, a ser a capital da recém-emancipada Província do Paraná. Desde então, a cidade, conhecida pelas suas ruas largas,[14] manteve um ritmo de crescimento urbano fortalecido pela chegada de diversos imigrantes europeus ao longo do século XIX, na maioria, alemães, poloneses, ucranianos e italianos,[15] que contribuíram para a atual diversidade cultural.";
+        break;
+
+        case "valor8":
+            document.getElementById('descricao').innerText = "Entre Rios é um município brasileiro do estado de Santa Catarina. Sua população estimada em 2004 era de 2.820 habitantes. Pertence a Região Geográfica Imediata de Xanxerê e a Região Geográfica Intermediária de Chapecó.";
+        break;
+
+        case "valor9":
+            document.getElementById('descricao').innerText = "Esta colônia foi fundada em 1878 por imigrantes italianos; sendo estes sobrevindos posteriormente por famílias alemãs, polonesas e açorianas.A pequena vila viria a se tornar um município somente 110 anos após sua fundação, 4 de Janeiro de 1988, desmembrada de Indaial . O município antes denominado Aquidaban está localizado no alto dos picos que circundam a região. No vale encontra-se uma cidade nas proximidades da rodovia BR-470.";
+        break;
+
+        case "valor10":
+            document.getElementById('descricao').innerText = "Lajeado é um município brasileiro do estado do Rio Grande do Sul, situado a 113 km da capital, Porto Alegre. Pertence à região geográfica intermediária Santa Cruz do Sul-Lajeado e à região imediata de Lajeado, sendo sua cidade mais populosa. A população estimada pelo IBGE em 2020 era de 85.033 habitantes, distribuídos em apenas 91,591 km²[4], o que torna Lajeado uma das dez cidades com maior densidade populacional do estado.[5]";
+        break;
+
+    }
+}
