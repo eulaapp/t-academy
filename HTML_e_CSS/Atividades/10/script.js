f39a7d18662dac9af9d8d330b5f2c94334562f83
eulaapp <pereiraeulapaula@gmail.com>
2022-10-26 11:08:43 -0300

    resolução atividade

--- HTML_e_CSS/Atividades/10/script.js
+++ HTML_e_CSS/Atividades/10/script.js
@@ -1,6 +1,8 @@
 let brenaVotos = 0
 let joaoVotos = 0
-
+let ralfVotos = 0
+let gustavoVotos = 0
+let nuloVotos = 0
 
 function votacao(voto) {
     let brena = document.getElementById("candidato1").value
@@ -10,23 +12,47 @@ function votacao(voto) {
     let nulo = document.getElementById("candidato5").value
 
     switch(voto) {
-        case brena:
+        case "Brena":
             brenaVotos++
             document.getElementById("brena").innerText = brenaVotos
         break;
-        case joao:
+        case "João":
             joaoVotos++
             document.getElementById("joao").innerText = joaoVotos
         break;
 
-        case ralf:
+        case "Ralf":
+            ralfVotos++
+            document.getElementById("ralf").innerText = ralfVotos
         break;
 
-        case gustavo:
+        case "Gustavo":
+            gustavoVotos++
+            document.getElementById("gustavo").innerText = gustavoVotos
         break;
 
-        case nulo:
+        case "Nulo":
+            nuloVotos++
+            document.getElementById("nulo").innerText = nuloVotos
         break;
     }
+
+    let totalVotos = brenaVotos+joaoVotos+ralfVotos+gustavoVotos+nuloVotos
+    document.getElementById("totalVotos").innerText = totalVotos
+
+    let candidatoMaisVotado = Math.max(brenaVotos,joaoVotos,ralfVotos,gustavoVotos,nuloVotos)
+
+    if (candidatoMaisVotado == brenaVotos) {
+        document.getElementById("candidatoMaisVotado").innerText = "Brena"
+    } else if (candidatoMaisVotado == joaoVotos) {
+        document.getElementById("candidatoMaisVotado").innerText = "João"
+    } else if (candidatoMaisVotado == ralfVotos) {
+        document.getElementById("candidatoMaisVotado").innerText = "Ralf"
+    } else if (candidatoMaisVotado == gustavoVotos) {
+        document.getElementById("candidatoMaisVotado").innerText = "Gustavo"
+    } else if (candidatoMaisVotado == nuloVotos) {
+        document.getElementById("candidatoMaisVotado").innerText = "Nulo"
+    }
+    
 
 }
