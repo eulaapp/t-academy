5683b5ef4963abfdb26895638e6afc5a4d1e7a38
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-04 16:34:42 -0300

    chamando função obterProdutos quando carrega a página, ajustes para inserir os dados na tabela que estavam incorretos

--- Spring/front-produto/script.js
+++ Spring/front-produto/script.js
@@ -1,33 +1,37 @@
 // vetor de produtos
 let vetor = [];
 
-// executar a função para obter os produtos
-obterProdutos()
+// ao carregar a página html
+window.onload = function() {
+    // armazenar os produtos no vetor
+    obterProdutos();
+}
 
 // obter todos os produtos
 function obterProdutos() {
     fetch("http://localhost:8080")
     .then(retorno => retorno.json())
     .then(produtos => vetor = produtos)
+    .then(produtos => listarProdutos())
 }
 
 // listar os produtos do vetor na tabela 
 function listarProdutos() {
     // obter a tabela
-    let table = document.getElementById("tabela");
+    let tabela = document.getElementById("tabela");
 
     // limpar conteúdo da tabela
-    table.innerHTML = "";
+    tabela.innerHTML = "";
 
     for(let i = 0; i < vetor.length; i++) {
         // criar linha
         let linha = tabela.insertRow(-1);
 
         // criar coluna
-        let colunaCodigo = tabela.insertCell(0);
-        let colunaNome = tabela.insertCell(1);
-        let colunaValor = tabela.insertCell(2);
-        let colunaSelecionar = tabela.insertCell(3);
+        let colunaCodigo = linha.insertCell(0);
+        let colunaNome = linha.insertCell(1);
+        let colunaValor = linha.insertCell(2);
+        let colunaSelecionar = linha.insertCell(3);
 
         // dados das colunas
         colunaCodigo.innerText = vetor[i].codigo;
