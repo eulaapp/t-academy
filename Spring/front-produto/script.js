1c5715a37ae6189678a1bc32553847f7e9d88fc7
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-04 16:24:44 -0300

    [ADD] função para listar os produtos

--- Spring/front-produto/script.js
+++ Spring/front-produto/script.js
@@ -10,3 +10,23 @@ function obterProdutos() {
     .then(retorno => retorno.json())
     .then(produtos => vetor = produtos)
 }
+
+// listar os produtos do vetor na tabela 
+function listarProdutos() {
+    // obter a tabela
+    let table = document.getElementById("tabela");
+
+    // limpar conteúdo da tabela
+    table.innerHTML = "";
+
+    for(let i = 0; i < vetor.length; i++) {
+        // criar linha
+        let linha = tabela.insertRow(-1);
+
+        // criar coluna
+        let colunaCodigo = tabela.insertCell(0);
+        let colunaNome = tabela.insertCell(1);
+        let colunaValor = tabela.insertCell(2);
+        let colunaSelecionar = tabela.insertCell(3);
+    }
+}
