dfa82d3f4cbd0d515213f4a5aed3ab06800cb85b
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-07 16:44:59 -0300

    exemplo usando lombok e validation

--- Spring/loja/src/main/java/br/com/projeto/loja/controle/LojaControle.java
+++ Spring/loja/src/main/java/br/com/projeto/loja/controle/LojaControle.java
@@ -1,5 +1,26 @@
 package br.com.projeto.loja.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.loja.modelo.LojaModelo;
+import br.com.projeto.loja.repositorio.LojaRepositorio;
+
+@RestController
 public class LojaControle {
-    
+    @Autowired
+    private LojaRepositorio acao;
+
+    @GetMapping("")
+    public Iterable<LojaModelo> listar() {
+        return acao.findAll();
+    }
+
+    @PostMapping("")
+    public LojaModelo cadastrar(@RequestBody LojaModelo obj) {
+        return acao.save(obj);
+    }
 }
--- Spring/loja/src/main/java/br/com/projeto/loja/modelo/LojaModelo.java
+++ Spring/loja/src/main/java/br/com/projeto/loja/modelo/LojaModelo.java
@@ -5,6 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +21,11 @@ public class LojaModelo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
+
+    @NotEmpty(message = "Informe o nome do produto")
+    @Length(min = 3, message = "O nome do produto precisa ter pelo menos três caracteres")
     private String produto;
+
+    @Min(value = 1, message = "O produto precisa custar pelo menos R$1,00")
     private double valor;
 }
