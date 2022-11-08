545f933a0963a6f8f9d852139e5fbd87f945a754
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-08 16:28:59 -0300

    [ADD] método para alterar artista

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/ArtistaControle.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/ArtistaControle.java
@@ -3,6 +3,7 @@ package br.com.atividade.atividade.controle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,11 @@ public class ArtistaControle {
     @GetMapping("")
     public Iterable<ArtistaModelo> selecionar() {
         return acao.findAll();
+    }
+
+    @PutMapping("")
+    public ArtistaModelo alterar(@RequestBody ArtistaModelo obj) {
+        return acao.save(obj);
     }
     
 }
