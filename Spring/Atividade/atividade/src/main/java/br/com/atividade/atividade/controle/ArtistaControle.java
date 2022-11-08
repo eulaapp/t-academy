1d3606e379a7fc29b7cb3af51d71e41aa94b3e0b
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-08 16:05:51 -0300

    [ADD] método para cadastrar artistas

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/ArtistaControle.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/ArtistaControle.java
@@ -1,5 +1,24 @@
 package br.com.atividade.atividade.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.atividade.atividade.modelo.ArtistaModelo;
+import br.com.atividade.atividade.repositorio.ArtistaRepositorio;
+
+@RestController
+@RequestMapping("/artistas")
 public class ArtistaControle {
+
+    @Autowired
+    private ArtistaRepositorio acao;
+
+    @PostMapping("")
+    public ArtistaModelo cadastrar(@RequestBody ArtistaModelo obj) {
+        return acao.save(obj);
+    }
     
 }
