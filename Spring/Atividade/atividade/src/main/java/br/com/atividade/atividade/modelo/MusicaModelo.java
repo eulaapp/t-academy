3f9e500bf110535d4591a135154fef8f2fb3b6a2
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 10:18:55 -0300

    [REMOVE] pacotes que não estavam sendo usados

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/modelo/MusicaModelo.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/modelo/MusicaModelo.java
@@ -1,8 +1,5 @@
 package br.com.atividade.atividade.modelo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
