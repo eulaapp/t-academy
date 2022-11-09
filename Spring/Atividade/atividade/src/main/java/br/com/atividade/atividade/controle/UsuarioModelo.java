f81cecf888f4279150461673fe354b883f0f1b84
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 10:24:35 -0300

    [REMOVE] arquivos que não serão usados

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/UsuarioModelo.java
+++
@@ -1,5 +0,0 @@
-package br.com.atividade.atividade.controle;
-
-public class UsuarioModelo {
-    
-}
--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/repositorio/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.atividade.atividade.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.atividade.atividade.modelo.UsuarioModelo;
-
-public interface UsuarioRepositorio extends CrudRepository<UsuarioModelo, Long> {
-    
-}
