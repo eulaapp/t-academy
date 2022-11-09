991ddf1f7d6228cf2cb6e58e47cd57a043c68e88
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 11:50:24 -0300

    [ADD] método para excluir playlist

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/PlaylistControle.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/PlaylistControle.java
@@ -1,6 +1,7 @@
 package br.com.atividade.atividade.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,10 @@ public class PlaylistControle {
     @PutMapping("/alterarNome/{codigo}")
     public PlaylistModelo editarPlaylist(@PathVariable long codigo, @RequestBody PlaylistModelo obj) {
         return acao.save(obj);
+    }
+
+    @DeleteMapping("{codigo}")
+    public void remover(@PathVariable long codigo) {
+        acao.deleteById(codigo);
     }
 }
