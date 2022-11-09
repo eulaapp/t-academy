8e03efbac55f3b94632d10d400a7c1000d7e4b2c
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 10:56:40 -0300

    método para cadastrar, listar e adicionar músicas na playlist

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/PlaylistControle.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/controle/PlaylistControle.java
@@ -1,5 +1,43 @@
 package br.com.atividade.atividade.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.atividade.atividade.modelo.MusicaModelo;
+import br.com.atividade.atividade.modelo.PlaylistModelo;
+import br.com.atividade.atividade.repositorio.PlaylistRepositorio;
+
+@RestController
+@RequestMapping("/playlists")
 public class PlaylistControle {
     
+    @Autowired
+    private PlaylistRepositorio acao;
+
+    @PostMapping("")
+    public PlaylistModelo cadastrar(@RequestBody PlaylistModelo obj) {
+        return acao.save(obj);
+    }
+
+    @GetMapping("")
+    public Iterable<PlaylistModelo> listar() {
+        return acao.findAll();
+    }
+
+    @PutMapping("/{codigo}")
+    public PlaylistModelo adicionarMusicaPlaylist(@PathVariable long codigo, @RequestBody MusicaModelo obj) {
+        PlaylistModelo pm = acao.findByCodigo(codigo);
+
+        pm.getMusicas().add(obj);
+
+        acao.save(pm);
+
+        return pm;
+    }
 }
