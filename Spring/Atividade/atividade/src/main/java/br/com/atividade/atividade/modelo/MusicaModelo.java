1ac2cc80a0728f806b6750488598c6fe59f273e1
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 10:17:03 -0300

    [REMOVE] atributo 'artista' e lista de artistas

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/modelo/MusicaModelo.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/modelo/MusicaModelo.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,12 +16,7 @@ public class MusicaModelo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long codigo;
     private String nome;
-    private String artista;
     private String link_video;
-
-    @ManyToMany(mappedBy = "musicas")
-    private List<ArtistaModelo> artistas = new ArrayList<>();
-
 
     public long getCodigo() {
         return this.codigo;
@@ -40,14 +34,6 @@ public class MusicaModelo {
         this.nome = nome;
     }
 
-    public String getArtista() {
-        return this.artista;
-    }
-
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
     public String getLink_video() {
         return this.link_video;
     }
@@ -55,13 +41,4 @@ public class MusicaModelo {
     public void setLink_video(String link_video) {
         this.link_video = link_video;
     }
-
-    public List<ArtistaModelo> getArtistas() {
-        return this.artistas;
-    }
-
-    public void setArtistas(List<ArtistaModelo> artistas) {
-        this.artistas = artistas;
-    }
-
 }
