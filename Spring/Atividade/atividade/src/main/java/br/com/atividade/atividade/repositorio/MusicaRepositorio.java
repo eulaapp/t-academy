f507cf9a2056d4e98bd1b84d81bb335ac66d35b5
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-09 10:21:21 -0300

    [ADD] método para pesquisar musica pelo nome

--- Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/repositorio/MusicaRepositorio.java
+++ Spring/Atividade/atividade/src/main/java/br/com/atividade/atividade/repositorio/MusicaRepositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.atividade.atividade.modelo.MusicaModelo;
 
 public interface MusicaRepositorio extends CrudRepository<MusicaModelo, Long> {
-    
+    Iterable<MusicaModelo> findByNomeContaining(String nomeMusica);
 }
