4a63f1ce31cbb0e1b72f2bb3967a081e088e09cb
Eula Paula Pereira <f0fp1411@t-systems.com.br>
2022-11-10 15:21:01 -0300

    [ADD] front-end atividade

---
+++ Spring/Atividade/front-end/sript.js
@@ -0,0 +1,92 @@
+let artistas = [];
+
+window.onload = function() {
+    fetchArtistas();
+}
+
+function fetchArtistas() {
+    fetch("http://localhost:8080/artistas")
+    .then(retorno => retorno.json())
+    .then(artisa => artistas = artisa)
+    .then(() => exibirArtistas())
+}
+
+function exibirArtistas() {
+    let tabela = document.getElementById("tabela");
+
+    tabela.innerHTML = "";
+
+    for(let i = 0; i < artistas.length; i++) {
+        let linha = tabela.insertRow(-1);
+
+        let colunaCodigo = linha.insertCell(0);
+        let colunaNome = linha.insertCell(1);
+        let colunaSelecionar = linha.insertCell(2);
+
+        colunaCodigo.innerText = artistas[i].codigo;
+        colunaNome.innerText = artistas[i].nome;
+        colunaSelecionar.innerHTML = `<button onclick="selecionarArtista(${artistas[i].codigo})" class="btn btn-success">Selecionar</button>`;
+
+    }
+}
+
+function cadastrarArtista() {
+    let nome = document.getElementById("artista").value;
+
+    if (nome.length < 3) {
+        alert("O nome deve conter ao menos três caracteres.")
+    } else {
+        let obj = {
+            "nome": nome
+        }
+
+        fetch("http://localhost:8080/artistas/cadastrar", {
+            method: "POST",
+            headers: {
+                "accpet": "application/json",
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+        .then(retorno => retorno.json())
+        .then(retornoConvertido => {
+            artistas.push(retornoConvertido);
+            exibirArtistas();
+        })
+
+    }
+}
+
+function selecionarArtista(codigo) {
+
+    fetch(`http://localhost:8080/artistas/filtrar/${codigo}`)
+    .then(retorno => retorno.json())
+    .then(retornoConvertido => {
+        
+        document.getElementById("btnCadastrar").style.display="none";
+
+        document.getElementById("btnAlterar").style.display="inline-block";
+        document.getElementById("btnRemover").style.display="inline-block";
+
+        document.getElementById("codigoArtista").value = retornoConvertido.codigo;
+        document.getElementById("artista").value = retornoConvertido.nome;
+    });
+
+}
+
+function removerArtista() {
+    let codigo = parseInt(document.getElementById("codigoArtista").value);
+
+    fetch(`http://localhost:8080/artistas/remover/${codigo}`,
+        {method: "DELETE"}
+    )
+    .then(() => {
+        let posicaoVetor = artistas.findIndex(objLinha => {
+            return objLinha.codigo == codigo
+        })
+
+        artistas.splice(posicaoVetor, 1);
+
+        exibirArtistas();
+    })
+}
